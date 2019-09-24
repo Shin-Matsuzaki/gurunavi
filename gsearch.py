1879810ee2dd69f00adb1312e889e587de658978
matsuzaki <shin.matsu.p@gmail.com>
2019-09-24 15:08:25 +0900

    ぐるなびAPIを用いてフリーワードで店舗検索するアプリケーションを実装

--- gsearch.py
+++ gsearch.py
@@ -4,11 +4,14 @@ import yaml
 
 def main():
     with open('key.yaml') as file:
-        API_KEY = yaml.load(file)['API_KEY']
+        key = yaml.load(file)
+        API_KEY = key['API_KEY']
+
+    Freeword = input('ワード検索>')
     # API_KEY = 'acf4e7e5c6089e5a360d1ddf1fcb4f0c'
     url = f'https://api.gnavi.co.jp/RestSearchAPI/v3'
     payload = {'keyid': API_KEY,
-               'freeword': 'ワイン',
+               'freeword': Freeword,
                'hit_per_page': 5}
 
     response = requests.get(url, params=payload)
